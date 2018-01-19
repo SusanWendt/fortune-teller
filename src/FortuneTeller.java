6c7f03045c19ea312bd8b8cee5a46ed32d122811
SusanWendt <wendt.37@osu.edu>
2018-01-19 10:18:06 -0500

    User input received through ROYGBIV

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class FortuneTeller {
 
@@ -10,10 +11,20 @@ public class FortuneTeller {
 		// [ ] If the user does not know what ROYGBIV is, 
 		// ask the user to enter “Help” to get a list of the ROYGBIV colors.
 		// [ ] Ask the user for the user’s number of siblings.
+		Scanner input = new Scanner(System.in);
+				
 		System.out.println("What is your first name?");
+		String firstName = input.nextLine().trim();
+		
 		System.out.println("What is your last name?");
+		String lastName = input.nextLine().trim();
+		
 		System.out.println("What is your age?");
+		int age = input.nextInt();
+		
 		System.out.println("What is the month of your birthday (in number format?)");
+		int birthMonth = input.nextInt();
+		
 		System.out.println("What is your favorite ROYGBIV color? (If you do not know what ROYGBIV is, please type \"HELP\".");
 		System.out.println("How many siblings do you have?");
 
