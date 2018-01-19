3839efc7cb2936b8504087f11cbd9cf3627b826d
SusanWendt <wendt.37@osu.edu>
2018-01-19 10:11:21 -0500

    Asking for user input

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -2,7 +2,20 @@
 public class FortuneTeller {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// [ ] Ask the user for the user’s first name.
+		// [ ] Ask the user for the user’s last name.
+		// [ ] Ask the user for the user’s age.
+		// [ ] Ask the user for the user’s birth month (as an ‘int’).
+		// [ ] Ask the user for the user’s favorite ROYGBIV color.
+		// [ ] If the user does not know what ROYGBIV is, 
+		// ask the user to enter “Help” to get a list of the ROYGBIV colors.
+		// [ ] Ask the user for the user’s number of siblings.
+		System.out.println("What is your first name?");
+		System.out.println("What is your last name?");
+		System.out.println("What is your age?");
+		System.out.println("What is the month of your birthday (in number format?)");
+		System.out.println("What is your favorite ROYGBIV color? (If you do not know what ROYGBIV is, please type \"HELP\".");
+		System.out.println("How many siblings do you have?");
 
 	}
 
